4892b3f61db5dfc8c99dc1e38b314635107e09b2
Natalia Bajek <natalia.bajek@bold.net.pl>
2019-10-23 10:48:57 +0200

    Updates

---
+++ precache-manifest.9efb3c331cf83d3b5a7977230cd39206.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db38b5e92303392f1776c52b652ee75c",
+    "url": "/circle-of-fifths/index.html"
+  },
+  {
+    "revision": "23c23732259eb32aa324",
+    "url": "/circle-of-fifths/static/css/main.d71698f8.chunk.css"
+  },
+  {
+    "revision": "5e1ff4e9afabd826dcd1",
+    "url": "/circle-of-fifths/static/js/2.9a335785.chunk.js"
+  },
+  {
+    "revision": "23c23732259eb32aa324",
+    "url": "/circle-of-fifths/static/js/main.9f6ba0d7.chunk.js"
+  },
+  {
+    "revision": "6d16e924795a1467fb82",
+    "url": "/circle-of-fifths/static/js/runtime-main.03f34c9d.js"
+  }
+]);
